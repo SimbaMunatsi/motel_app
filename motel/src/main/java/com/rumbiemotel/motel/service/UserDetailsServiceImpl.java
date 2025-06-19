5b94f992c9725444228f802f991065f9f299f311
SimbaMunatsi <vsmunatsi@gmail.com>
2025-06-19 14:04:04 +0200

    implemented UserDetailsService

---
+++ motel/src/main/java/com/rumbiemotel/motel/service/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.rumbiemotel.motel.service;
+
+import com.rumbiemotel.motel.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+    }
+}
